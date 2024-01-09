b906319055c282a03f4a24c48b1fdfc088635f48
Daniel Rabottini <daniel.rabottini@edu.unito.it>
2024-01-09 16:22:34 +0100

    Create project express

---
+++ RabottiniAbouridaRemondino/solution/public/javascripts/homepage.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const barraRicerca = document.getElementById('barra-ricerca');
+    const mostraBarra = document.getElementById('mostra-barra');
+
+    const opzioniLogin = document.getElementById('opzioni-login');
+    const mostraOpzioni = document.getElementById('mostra-opzioni');
+
+    const containerW1 = document.getElementById('wrap-w-items1');
+    const containerW2 = document.getElementById('wrap-w-items2');
+    const mostraAltroW = document.getElementById('btn-switch-w');
+
+    const containerN1 = document.getElementById('wrap-n-items1');
+    const containerN2 = document.getElementById('wrap-n-items2');
+    const mostraAltroN = document.getElementById('btn-switch-n');
+
+
+    mostraBarra.addEventListener('click', function() {
+        if(!barraRicerca.classList.contains('visible')) {
+            barraRicerca.classList.remove('hidden');
+            barraRicerca.classList.add('visible');
+        } else {
+            barraRicerca.classList.remove('visible');
+            barraRicerca.classList.add('hidden');
+        }
+    });
+
+    mostraOpzioni.addEventListener('click', function() {
+        if(!opzioniLogin.classList.contains('visible')) {
+            opzioniLogin.classList.remove('hidden');
+            opzioniLogin.classList.add('visible');
+        } else {
+            opzioniLogin.classList.remove('visible');
+            opzioniLogin.classList.add('hidden');
+        }
+    });
+
+    mostraAltroW.addEventListener('click', function () {
+        if(!containerW2.classList.contains('visible-wn')) {
+            containerW1.classList.remove('visible-wn');
+            containerW1.classList.add('hidden-wn');
+            containerW2.classList.remove('hidden-wn');
+            containerW2.classList.add('visible-wn');
+        } else {
+            containerW2.classList.remove('visible-wn');
+            containerW2.classList.add('hidden-wn');
+            containerW1.classList.remove('hidden-wn');
+            containerW1.classList.add('visible-wn');
+        }
+    });
+
+    mostraAltroN.addEventListener('click', function () {
+        if(!containerN2.classList.contains('visible-wn')) {
+            containerN1.classList.remove('visible-wn');
+            containerN1.classList.add('hidden-wn');
+            containerN2.classList.remove('hidden-wn');
+            containerN2.classList.add('visible-wn');
+        } else {
+            containerN2.classList.remove('visible-wn');
+            containerN2.classList.add('hidden-wn');
+            containerN1.classList.remove('hidden-wn');
+            containerN1.classList.add('visible-wn');
+        }
+    });
+
+});
