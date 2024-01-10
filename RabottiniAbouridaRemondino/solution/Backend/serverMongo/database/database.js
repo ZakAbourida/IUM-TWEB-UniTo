9ccbac487b87e29e42c7be61dca1cd34d91c4724
Zakaria Abourida <zakaria.abourida@edu.unito.it>
2024-01-10 22:08:52 +0100

    piccola correzzione per la connessione del database

--- RabottiniAbouridaRemondino/solution/Backend/serverMongo/database/database.js
+++ RabottiniAbouridaRemondino/solution/Backend/serverMongo/database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb://localhost:27017/DynamicDatabase';
+const mongoDB = 'mongodb://localhost:27017';
 mongoose.Promise = global.Promise;
 connection = mongoose.connect(mongoDB, {
     useNewUrlParser: true,
